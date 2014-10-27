2b1bd8812cd585018b9e3fb43141bd66c92be709
daxiao <369057944@qq.com>
2014-10-27 16:41:20 +0800

    daxiao

--- src/com/liangliangshi/kaka/http/URLs.java
+++ src/com/liangliangshi/kaka/http/URLs.java
@@ -2,12 +2,12 @@ package com.liangliangshi.kaka.http;
 
 public class URLs {
 
-	public final static String HOST = "http://192.168.1.104/KaKa/";
+	public final static String HOST = "http://localhost/index.php/Home/Index/";
 	
 //	public final static String APP_DOWNLOAD = "http://121.40.82.49:88/app/android/deviceoo_download_info.json";
 	
 	public final static String USERLOGIN = HOST+"userlogin";//µÇÂ½
 
-	public final static String TEST = HOST+"HttpTest/newfile.php";//µÇÂ½
+	public final static String TEST = HOST+"getuser";//µÇÂ½
 	
 }
